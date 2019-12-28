1a23b56cc47a019ebd5f426a6a0682f71ae1b14f
Dan Hoover <mudmin@gmail.com>
2019-12-28 05:04:50 -0500

    commit

--- src/membership/hooks/accountbody.php
+++ src/membership/hooks/accountbody.php
@@ -1,5 +1,7 @@
 <?php if(count(get_included_files()) ==1) die(); //Direct Access Not Permitted
 $memSettings = $db->query("SELECT * FROM plg_mem_settings")->first();
 if($memSettings->payments == 1){?>
-<button type="button" onclick="window.location.href = 'account.php?change=membership';" name="button" class="btn btn-primary">Manage Membership</button>
+<div class="form-group">
+  <button type="button" onclick="window.location.href = 'account.php?change=membership';" name="button" class="btn btn-primary">Manage Membership</button>
+</div>
 <?php } ?>
