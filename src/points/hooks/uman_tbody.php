c33bbd8fbb9b91bd7e0ab13888a04aeba431e0d4
unknown <mudmin@gmail.com>
2020-02-07 13:34:54 -0500

    points 1.0.5

--- src/points/hooks/uman_tbody.php
+++ src/points/hooks/uman_tbody.php
@@ -1,3 +1,4 @@
 <?php if(count(get_included_files()) ==1) die(); //Direct Access Not Permitted Leave this line in place
-global $user;?>
-<td><?=pointsUnitReturn($user->data()->plg_points);?></td>
+global $v1;
+?>
+<td><?=pointsUnitReturn($v1->plg_points);?></td>
