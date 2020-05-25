d71a5d8099f26fb1073eecda574c4482d3fffe77
Dan Hoover <mudmin@gmail.com>
2020-05-25 06:11:46 -0400

    API Builder 1.0.3

--- src/apibuilder/hooks/accountbottom.php
+++ src/apibuilder/hooks/accountbottom.php
@@ -20,14 +20,12 @@ if($apisettings->api_auth_type == 4 || $apisettings->api_auth_type == 5){//  ?>
   Your API Key: <?=$user->data()->apibld_key?>
   <input type="submit" name="genNewkey" value="Generate New Key">
 </form>
-<?php if($apisettings->api_auth_type == 4 || $apisettings->api_auth_type == 5){//  ?>
+<?php if($apisettings->api_auth_type == 5){//  ?>
   <form class="" action="" method="post">
     Your Key Is Locked To: <input type="text" name="ip" value="<?=$user->data()->apibld_ip?>">
     <input type="submit" name="updateKeyIP" value="Update IP Hostname"><br>
     In order to use the API, we need to know what IP address your requests are coming from.  If you have a Static IP, you can store that above.  If you have an IP address that changes, we recommend you use a free service like duckdns.org and put your url in the box above in the format of whatever.duckdns.org.
   </form>
-
-
 <?php
 }
 }
