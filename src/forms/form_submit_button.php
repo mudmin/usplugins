4f16d2d585f02134dfcd50fda180661d24f2b7c7
mudmin@gmail.com <mudmin@gmail.com>
2021-06-04 09:27:38 -0400

    Form Builder 1.2.2

--- src/forms/form_submit_button.php
+++ src/forms/form_submit_button.php
@@ -5,8 +5,4 @@ if(!isset($opts['submit']) && !isset($opts['nosubmit'])){ ?>
   //do nothing
 }else{
    ?>
-  <input type="submit" name="
-  <?php if($opts['submit'] != ''){echo $opts['submit'];}else{echo "submit";}?>
-  " value="<?php if($opts['value'] != ''){echo $opts['value'];}else{echo "Submit";}?>" class="
-  <?php if($opts['class'] != ''){echo $opts['class'];}else{echo "btn btn-primary";}?>">
-<?php }//end submit ?>
+  <input type="submit" name="<?php if($opts['submit'] != ''){echo $opts['submit'];}else{echo "submit";}?>" value="<?php if($opts['value'] != ''){echo $opts['value'];}else{echo "Submit";}?>" class="<?php if($opts['class'] != ''){echo $opts['class'];}else{echo "btn btn-primary";}?>"><?php }//end submit ?>
