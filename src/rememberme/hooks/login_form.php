05cc938a205a74baa73ed4a84f9076884fbd3492
mudmin@gmail.com <mudmin@gmail.com>
2021-10-11 12:12:56 -0400

    remember me 1.0.0

---
+++ src/rememberme/hooks/login_form.php
@@ -0,0 +1,5 @@
+<?php if(count(get_included_files()) ==1) die(); //Direct Access Not Permitted ?>
+<div class="form-group">
+  <label for="remember">
+    <input type="checkbox" name="remember" id="remember" > <?=lang("SIGNIN_REMEMBER")?></label>
+</div>
