58f1c2bca16f8021df30b8d345927f91d22e8ef4
Dan Hoover <mudmin@gmail.com>
2023-05-22 08:00:55 -0400

    performance checker

--- src/performancechecker/configure.php
+++ src/performancechecker/configure.php
@@ -1,4 +1,4 @@
-  <?php if(!in_array($user->data()->id,$master_account)){ Redirect::to($us_url_root.'users/admin.php');} //only allow master accounts to manage plugins! ?>
+<?php if(!in_array($user->data()->id,$master_account)){ Redirect::to($us_url_root.'users/admin.php');} //only allow master accounts to manage plugins! ?>
 
 <?php
 include "plugin_info.php";
@@ -18,7 +18,16 @@ if(!Token::check($token)){
  			<div class="col-sm-12">
           <a href="<?=$us_url_root?>users/admin.php?view=plugins">Return to the Plugin Manager</a>
  					<h1>Welcome to the Performance Checker Plugin!</h1><br>
-          <p>This plugin checks your page load times.  If you would like to help make it more powerful, come talk to us on the Discord channel. </p>
-          <p>- The UserSpice Team</p>
+          <p class="mb-3">This plugin checks your page load times and displays the overall page load time on the page.  There are 2 additional functions. <code>startPageTimer()</code> and <code>checkPageTimer($id = "", $user_id = 0)</code>
+        
+          </p>
+          <p class="mb-3">These functions are great for checking the running page load time throguhout the page. Simply call startPageTimer() at the top of your page and call <code>checkPageTimer("uniqueIDHere")</code> at any point in the page where you want to see the elapsed time since the page started loading and since your last call.  By passing your own unique id, it will make it really easy to figure out between which two calls of checkPageTimer your performance issue is happening. </p>
+          
+          <p class="mb-3">By passing an optional user id as the second parameter, only that user will see the debugging info, which is great if you're debugging on a live site.</p>
+      
+          <p class="mb-3">
+          If you appreciate this plugin and would like to make a donation to the author, you can do so at <a href="https://UserSpice.com/donate">https://UserSpice.com/donate</a>. Either way, thanks for using UserSpice!
+          </p>
+
  			</div> <!-- /.col -->
  		</div> <!-- /.row -->
--- src/performancechecker/functions.php
+++ src/performancechecker/functions.php
@@ -10,3 +10,52 @@
 // if(!function_exists('performancecheckerFunction')) {
 //   function performancecheckerFunction(){ }
 // }
+
+if(!function_exists("startPageTimer")){
+function startPageTimer()
+{   
+    if(isset($_SESSION['pageTimer'])){
+      unset($_SESSION['pageTimer']);
+    }
+    return microtime(true);
+}
+}
+
+//pass a string to identify the call
+if(!function_exists("checkPageTimer")){
+function checkPageTimer($id = "", $user_id = 0){    
+    global $user;
+    if($user_id > 0){
+        if(!isset($user->data()->id) || $user->data()->id != $user_id) {
+          return;
+        }
+     
+    }
+
+
+    $currentTime = microtime(true);
+
+    if (!isset($_SESSION['pageTimer'])) {
+        $_SESSION['pageTimer'] = [
+            'startTime' => $currentTime,
+            'lastTime' => $currentTime,
+
+        ];
+    }
+
+    $pageTimer = $_SESSION['pageTimer'];
+
+    $executionTime = round(($currentTime - $pageTimer['startTime']) * 1000, 2); // Time since page started loading
+    $timeSinceLastCall = round(($currentTime - $pageTimer['lastTime']) * 1000, 2); // Time since last call to stopTimer
+
+    $_SESSION['pageTimer']['lastTime'] = $currentTime;
+
+    echo "<h5>Time since last call: <span class='text-danger'>{$timeSinceLastCall} ms</span> - <span class='text-success'>{$executionTime} ms </span> since page load";
+
+
+    if($id != ""){
+      echo " - Call id:  $id ";
+    }
+    echo "</h5>";
+}
+}
