3727107338ee908d13a1e06c1688f1fc7ce8a456
Dan Hoover <mudmin@gmail.com>
2024-05-28 04:14:13 -0400

    commit ryzen9

--- src/html_purifier/footer.php
+++
@@ -1,3 +0,0 @@
-<?php
-//Please don't load code on the footer of every page if you don't need it on the footer of every page.
-//bold("<br>HTML Purifier Footer Loaded");
--- src/html_purifier/purifier.php
+++
@@ -1,2 +0,0 @@
-<?php
-require_once $abs_us_root . $us_url_root . "usersc/plugins/html_purifier/assets/library/HTMLPurifier.auto.php";
