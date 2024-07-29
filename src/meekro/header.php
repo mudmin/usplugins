ef15c23f7cac538b45d587acdc9aa3536413ed6f
Dan Hoover <mudmin@gmail.com>
2024-07-29 06:22:29 -0400

    meekro 2.3.1

--- src/meekro/header.php
+++ src/meekro/header.php
@@ -2,7 +2,18 @@
 //Please don't load code on the header of every page if you don't need it on the header of every page.
 require_once $abs_us_root.$us_url_root.'usersc/plugins/meekro/assets/meekrodb.2.3.class.php';
 // if($settings->meekro == 0){
-$mdb = new MeekroDB($config['mysql']['host'], $config['mysql']['username'], $config['mysql']['password'], $config['mysql']['db']);
+  $raw_sql_host = explode(";",$config['mysql']['host']);
+
+  $config['mysql']['clean_host'] =  $raw_sql_host[0];
+
+  $mdbfields['port'] = 3306;
+  if(isset($raw_sql_host[1])){
+    $mdbfields['port'] = $raw_sql_host[1];
+    $mdbfields['port'] = str_replace(';', '', $mdbfields['port']);
+    $mdbfields['port'] = (int)(trim(str_replace("port=","",$mdbfields['port'])));
+  }
+  
+  $mdb = new MeekroDB($config['mysql']['clean_host'], $config['mysql']['username'], $config['mysql']['password'], $config['mysql']['db'], $mdbfields['port']);
 // }
 
 // I'm currently working with the MeekroDB Developer to fix this.  Right now, we cannot use the static method.
