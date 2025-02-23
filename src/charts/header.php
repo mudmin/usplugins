93eab371ea0168e06bbb53d737862679a80363ac
mudmin <mudmin@gmail.com>
2025-02-23 11:16:55 -0500

    update charts plugin

--- src/charts/header.php
+++ src/charts/header.php
@@ -2,4 +2,4 @@
 //Please don't load code on the header of every page if you don't need it on the header of every page.
 // bold("<br>Charts Header.php Loaded");
 ?>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.min.js" integrity="sha512-L0Shl7nXXzIlBSUUPpxrokqq4ojqgZFQczTYlGjzONGTDAcLremjwaWv5A+EDLnxhQzY5xUZPWLOLqYRkY0Cbw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
