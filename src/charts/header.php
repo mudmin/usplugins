a194669f4db4e35d06aa367bc2ffd0513da205cf
mudmin <mudmin@gmail.com>
2025-02-24 05:45:52 -0500

    charts 1.0.6

--- src/charts/header.php
+++ src/charts/header.php
@@ -2,4 +2,4 @@
 //Please don't load code on the header of every page if you don't need it on the header of every page.
 // bold("<br>Charts Header.php Loaded");
 ?>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.min.js" integrity="sha512-L0Shl7nXXzIlBSUUPpxrokqq4ojqgZFQczTYlGjzONGTDAcLremjwaWv5A+EDLnxhQzY5xUZPWLOLqYRkY0Cbw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.min.js" integrity="sha512-CQBWl4fJHWbryGE+Pc7UAxWMUMNMWzWxF4SQo9CgkJIN1kx6djDQZjh3Y8SZ1d+6I+1zze6Z7kHXO7q3UyZAWw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
