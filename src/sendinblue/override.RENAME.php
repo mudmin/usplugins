240a2a7d3f73687d1cbb7097f18bbd6bfce50789
Dan Hoover <mudmin@gmail.com>
2026-05-17 05:23:03 -0400

    Release sendinblue 1.5.1 - override.php passes reply_name to the Brevo reply-to display name and converts the email() attachment path into a Brevo attachment

--- src/sendinblue/override.RENAME.php
+++ src/sendinblue/override.RENAME.php
@@ -3,8 +3,19 @@
 function email($to, $subject, $body, $opts = [], $attachment = null)
 {
     $brevo_opts = [];
-    if (isset($opts['replyTo'])) { $brevo_opts['reply']     = $opts['replyTo']; }
-    if (isset($opts['email']))   { $brevo_opts['from']      = $opts['email']; }
-    if (isset($opts['name']))    { $brevo_opts['from_name'] = $opts['name']; }
+    if (isset($opts['replyTo']))    { $brevo_opts['reply']      = $opts['replyTo']; }
+    if (isset($opts['email']))      { $brevo_opts['from']       = $opts['email']; }
+    if (isset($opts['name']))       { $brevo_opts['from_name']  = $opts['name']; }
+    if (isset($opts['reply_name'])) { $brevo_opts['reply_name'] = $opts['reply_name']; }
+
+    if ($attachment !== null) {
+        $content = @file_get_contents($attachment);
+        if ($content !== false) {
+            $brevo_opts['attachments'] = [
+                ['content' => base64_encode($content), 'name' => basename($attachment)]
+            ];
+        }
+    }
+
     return sendinblue($to, $subject, $body, '', $brevo_opts);
 }
